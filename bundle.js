b0826a3f1975af634dcc5f4be55e0015119ac3af
Aditya Kadam <aditya@Adityas-MacBook-Pro.local>
2020-09-02 21:24:29 +0530

    removed console logs

--- bundle.js
+++ bundle.js
@@ -22991,33 +22991,27 @@ $('#submit').on('click', function(e) {
 
             if(header.indexOf(el.tagName) > -1) {
               selectedTableBody.find('td').eq(index).append(`${$(el).text()} <br><br>`)
-              console.log(`<${el.tagName}>${$(el).text()}</${el.tagName}>`)
             }
 
             if(el.tagName === 'a') {
               selectedTableBody.find('td').eq(index).append(document.createTextNode(`<a href='${$(el).attr('href')}'>${$(el).text()}</a>`)).append('<br>')
-              console.log(`<a href='${$(el).attr('href')}'>${$(el).text()}</a>`)
             }
 
             if(el.tagName === 'img') {
               selectedTableBody.find('td').eq(index).append(document.createTextNode(`<img src='${$(el).attr('src')}' alt='${$(el).attr('alt')}'/>`)).append('<br>')
-              console.log(`<img src='${$(el).attr('src')}' alt='${$(el).attr('alt')}'/>`)
             }
 
             if(el.tagName === 'link') {
               if ($(el).attr('rel') === 'canonical') {
                 selectedTableBody.find('td').eq(index).append(document.createTextNode(`<link rel='canonical' href='${$(el).attr('href')}'/>`)).append('<br>')
-                console.log(`<link rel='canonical' href='${$(el).attr('href')}'/>`)
               }
             }
             
             if(el.tagName === 'meta') {
               if($(el).attr('name')) {
                 selectedTableBody.find('td').eq(index).append(document.createTextNode(`<meta name='${$(el).attr('name')}' content='${$(el).attr('content')}'/>`)).append('<br>')
-                console.log(`<meta name='${$(el).attr('name')}' content='${$(el).attr('content')}'/>`)
               } else if($(el).attr('property')){
                 selectedTableBody.find('td').eq(index).append(document.createTextNode(`<meta property='${$(el).attr('property')}' content='${$(el).attr('content')}'/>`)).append('<br>')
-                console.log(`<meta property='${$(el).attr('property')}' content='${$(el).attr('content')}'/>`)
               }              
             }
           })
