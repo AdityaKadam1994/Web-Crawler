7f1c524339928c42f2871935e428d47da701adc7
Aditya Kadam <aditya@Adityas-MacBook-Pro.local>
2020-09-02 21:22:42 +0530

    Output generated in table

--- bundle.js
+++ bundle.js
@@ -22972,37 +22972,51 @@ $('#submit').on('click', function(e) {
         tags.push($(this).val())
       })
 
+      const selectedTable = $('#output-table')
+      const selectedTableHeaders = $('#output-table thead tr')
+      const selectedTableBody = $('#output-table tbody tr')
       //Api call to links
+      
       await axios.get(`${link}`).then(res => {
         const $ = cheerio.load(res.data)
         const header = ['h1','h2','h3','h4','h5','h6']
 
+        selectedTable.css('color','white')
+        
         //Looping on tags
-        tags.forEach(selectedTags => {
+        tags.forEach((selectedTags, index) => {
+          selectedTableHeaders.append(`<th>${selectedTags}</th>`)
+          selectedTableBody.append('<td></td>')
           $(selectedTags).each((ind, el) => {
 
             if(header.indexOf(el.tagName) > -1) {
+              selectedTableBody.find('td').eq(index).append(`${$(el).text()} <br><br>`)
               console.log(`<${el.tagName}>${$(el).text()}</${el.tagName}>`)
             }
 
             if(el.tagName === 'a') {
+              selectedTableBody.find('td').eq(index).append(document.createTextNode(`<a href='${$(el).attr('href')}'>${$(el).text()}</a>`)).append('<br>')
               console.log(`<a href='${$(el).attr('href')}'>${$(el).text()}</a>`)
             }
 
             if(el.tagName === 'img') {
+              selectedTableBody.find('td').eq(index).append(document.createTextNode(`<img src='${$(el).attr('src')}' alt='${$(el).attr('alt')}'/>`)).append('<br>')
               console.log(`<img src='${$(el).attr('src')}' alt='${$(el).attr('alt')}'/>`)
             }
 
             if(el.tagName === 'link') {
               if ($(el).attr('rel') === 'canonical') {
+                selectedTableBody.find('td').eq(index).append(document.createTextNode(`<link rel='canonical' href='${$(el).attr('href')}'/>`)).append('<br>')
                 console.log(`<link rel='canonical' href='${$(el).attr('href')}'/>`)
               }
             }
             
             if(el.tagName === 'meta') {
               if($(el).attr('name')) {
+                selectedTableBody.find('td').eq(index).append(document.createTextNode(`<meta name='${$(el).attr('name')}' content='${$(el).attr('content')}'/>`)).append('<br>')
                 console.log(`<meta name='${$(el).attr('name')}' content='${$(el).attr('content')}'/>`)
               } else if($(el).attr('property')){
+                selectedTableBody.find('td').eq(index).append(document.createTextNode(`<meta property='${$(el).attr('property')}' content='${$(el).attr('content')}'/>`)).append('<br>')
                 console.log(`<meta property='${$(el).attr('property')}' content='${$(el).attr('content')}'/>`)
               }              
             }
