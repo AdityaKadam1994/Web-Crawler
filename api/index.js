3bb564d6817965425334effc2082815838c565f4
VinodNimbalkar <omvinod@yahoo.com>
2020-09-05 11:10:50 +0530

    cors enable and return data in array instead of object

--- api/index.js
+++ api/index.js
@@ -4,8 +4,15 @@ module.exports = async (req, res) => {
   try {
     const { urls, tags } = req.body;
     let collectData = await Promise.all(urls.map(async url => await scrapUrl(url, tags)))
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.send(collectData)
   } catch (error) {
-    res.send(error)
+    res.status(500)
+    const response = error.response || {}
+    res.send({
+      message: error.message,
+      response
+    })
   }
 }
