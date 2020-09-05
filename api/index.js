1c0c04db7f4ad11046e38ed20d7b50b80fb7baa2
VinodNimbalkar <omvinod@yahoo.com>
2020-09-05 14:45:52 +0530

    :bug: cors enabled bug fixed

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 const scrapUrl = require('./helper');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   try {
     const { urls, tags } = req.body;
     let collectData = await Promise.all(urls.map(async url => await scrapUrl(url, tags)))
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.send(collectData)
   } catch (error) {
     res.status(500)
